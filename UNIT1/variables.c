0d59447764f8171d766cb91c851c6569d9ebd279
AdalbertoJimenez18 <2109085@upy.edu.mx>
2022-02-14 17:10:51 -0600

    coded finished

---
+++ UNIT1/variables.c
@@ -0,0 +1,35 @@
+#include <stdio.h> //its your moment.
+
+//DataType:
+//Create a variable: dataType anyName: | dataType anyName= 12; //Solo se declara una vez
+
+int myInt = 24; //-> text format: %i, %d
+float myFloat = 22.7 //-> text format: %i, %d
+char myChar = 'a'; // -> %c
+int myArray[7] = { 5, 27, 47, 12, 8, 7, 0 };
+              //  0  1   2   3   4  5  6 ...n
+float myFloatarray[255] = { 1.33, 2.55, 666.1 }
+char word[10] = { 'b', 'e', 't', 'o' };
+char country[10] "México"; //%s
+char aName = "Adalberto" //%s
+int main(){
+
+      printf("hola clase, myInt = %d\n", myInt);
+      myInt = 25;
+      printf("hola clase, myInt = %d\n", myInt);
+      int yearsOld = 18;
+      printf("%f\n", myFloatarray[1]);
+      printf("%c\n", myChar);
+      myChar = 'i';
+      printf("%c\n", myChar);
+      country[0] = "i";
+      country[1] = "t";
+      country[2] = "a";
+      country[3] = "l";
+      country[4] = "y";
+      aName = "Adalberto"
+      
+
+
+      return 0;
+}
