397a4d35b6ca564447b927d866dfdbbe8c740a72
AdalbertoJimenez18 <2109085@upy.edu.mx>
2022-02-14 16:22:06 -0600

    added general struct

---
+++ UNIT1/viariables.c
@@ -0,0 +1,12 @@
+#include <stdio.h> //its your moment.
+
+//DataType:
+//create a variable: dataType anyName: | dataType anyName= 12; //Solo se declara una vez
+
+int myInt = 24; //-> text format: %i, %d
+float myFloat = 22.7
+
+int main(){
+
+      return 0;
+}
